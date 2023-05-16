22adcde8a98fb5e2339211eae10ae199d88a4c6d
Juan Carlos Torrico <samieltx@gmail.com>
2023-05-16 11:01:23 -0400

    eliminacion de nulos en la columna publish_date

--- tienda_amiga/transform_jct.py
+++ tienda_amiga/transform_jct.py
@@ -4,5 +4,9 @@ from tienda_amiga.cfg import DATA_DIR
 def transform(fecha: str):
     # TODO: Apply transformations
     df = pd.read_csv(DATA_DIR / "data_bitcoin.csv",usecols=['source_url','url','title','text','tags','authors','publish_date'])    
+    df = df.dropna()    
+    df['publish_date'] = pd.to_datetime(df['publish_date'])
+    df['publish_date_f'] = (df['publish_date'])[0:10]
+    df['publish_hour'] = (df['publish_date'])[11:8]
     df['fecha']=fecha
     df.to_csv(DATA_DIR / "data_bitcoin_transformed.csv", index=False)
